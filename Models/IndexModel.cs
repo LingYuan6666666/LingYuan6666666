121e38c7281ac9c008567851a4eea1a04c9fcb4f
LingYuan6666666 <47761997+LingYuan6666666@users.noreply.github.com>
2019-03-18 23:51:17 +0800

    Add files via upload

---
+++ Models/IndexModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VotingSystem.data;
+
+namespace VotingSystem.Models
+{
+    public class IndexModel
+    {
+        public IEnumerable<User_Recording> User_Recording { get; set; }
+        public IEnumerable<Topic_Recording> Topic_Recording { get; set; }
+        public IndexModel()
+        {
+            VotingSystemEntities db = new VotingSystemEntities();
+            this.User_Recording = db.User_Recording.ToList();
+            this.Topic_Recording = db.Topic_Recording.ToList();
+
+        }
+    }
+}
